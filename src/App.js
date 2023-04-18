7826b2a9e6ac50f0839c66685965560f46172c5d
sudeepgud <sudeepgudipalli@gmail.com>
2023-04-18 15:00:06 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ import SignUp from './Components/SignUp';
 import Predict from './Components/Predict';
 
 export default function App() {
+  let hours = 1;
+  let now = new Date().getTime();
+  let setupTime = localStorage.getItem('setupTime');
+  if(setupTime == null) {
+    localStorage.setItem('setupTime', now)
+  }else {
+    if(now-setupTime > hours*60*1000) {
+        localStorage.clear()
+        localStorage.setItem('setupTime', now);
+    }
+}
   return (
     <Router>
       <Routes>
